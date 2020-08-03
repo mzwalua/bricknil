38b800316f6ba78d54671dbc0b3350d843a50e62
Wilfried Holzke <gentoo@holzke.net>
2020-08-03 18:12:02 +0200

    Added IMU sensors to __init__.py

--- bricknil/sensor/__init__.py
+++ bricknil/sensor/__init__.py
@@ -3,6 +3,7 @@ from .sensor import \
         VisionSensor, InternalTiltSensor, ExternalMotionSensor, \
         ExternalTiltSensor, RemoteButtons, Button, \
         CurrentSensor, VoltageSensor,  \
+        PoweredUpHubIMUPosition, PoweredUpHubIMUGyro, PoweredUpHubIMUAccelerometer, \
         DuploSpeedSensor, DuploVisionSensor
 
 from .motor import InternalMotor, ExternalMotor, TrainMotor, WedoMotor, DuploTrainMotor
